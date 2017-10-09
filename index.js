7951617fd3459b4f75b2a93cae3e7e16bbcfd686
Tim Allen <tim@noblesamurai.com>
2017-10-09 13:50:26 +1100

    semistandard compliance.

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-var amqp = require('amqplib'),
-    events = require('events');
+var amqp = require('amqplib');
+var events = require('events');
 
-module.exports = function(amqpUrl, exchange, routingKey, message) {
+module.exports = function (amqpUrl, exchange, routingKey, message) {
   var emitter = new events.EventEmitter();
-  process.nextTick(function() {
-
+  process.nextTick(function () {
     // Connect to the AMQP server
-    amqp.connect(amqpUrl).then(function(conn) {
+    amqp.connect(amqpUrl).then(function (conn) {
       // Need a channel to talk to it over
-      conn.createChannel().then(function(ch) {
+      conn.createChannel().then(function (ch) {
         // Create the queue we'll listen for the response on.
         // ch.assertQueue with blank arg will assert random named queue, used
         // for replyTo.
-        ch.assertQueue().then(function(asserted) {
+        ch.assertQueue().then(function (asserted) {
           ch.bindQueue(asserted.queue, exchange, asserted.queue); // Bind response queue so it gets response.
           ch.publish(exchange, routingKey, new Buffer(JSON.stringify(message)), { replyTo: asserted.queue });
           ch.consume(asserted.queue, onMessage);
         });
 
-        function onMessage(message) {
+        function onMessage (message) {
           var payload = JSON.parse(message.content.toString());
           ch.ack(message);
           if (payload.complete) conn.close();
--- test/index.js
+++ test/index.js
@@ -1,30 +1,37 @@
-var expect = require('expect.js'),
-    amqplibRpcClient = require('..'),
-    amqp = require('amqplib'),
-    amqpUrl = process.env.AMQP_URL || 'amqp://localhost',
-    exchange = 'test';
+var expect = require('expect.js');
+var amqplibRpcClient = require('..');
+var amqp = require('amqplib');
+var amqpUrl = process.env.AMQP_URL || 'amqp://localhost';
+var exchange = 'test';
 
-before(function() {
-  return amqp.connect(amqpUrl).then(function(conn) {
-    conn.createChannel().then(function(ch) {
-      ch.assertExchange(exchange);
-      ch.assertQueue('add');
-      ch.bindQueue('add', exchange, 'add');
-      return ch.consume('add', onMessage);
+describe('amqplib-rpc-client', function () {
+  var conn;
+  before(function () {
+    return amqp.connect(amqpUrl).then(function (_conn) {
+      conn = _conn;
+      conn.createChannel().then(function (ch) {
+        ch.assertExchange(exchange);
+        ch.assertQueue('add');
+        ch.bindQueue('add', exchange, 'add');
+        return ch.consume('add', onMessage);
 
-      function onMessage(message) {
-        var payload = JSON.parse(message.content.toString());
-        var response = { type: 'data', answer: payload.number1 + payload.number2 };
-        ch.publish(exchange, message.properties.replyTo, new Buffer(JSON.stringify(response)));
-        ch.ack(message);
-      }
+        function onMessage (message) {
+          var payload = JSON.parse(message.content.toString());
+          var response = { type: 'data', answer: payload.number1 + payload.number2 };
+          ch.publish(exchange, message.properties.replyTo, new Buffer(JSON.stringify(response)));
+          ch.ack(message);
+        }
+      });
     });
   });
-});
 
-describe('amqplib-rpc-client', function() {
-  it('should send and receive a response', function(done) {
-    amqplibRpcClient(amqpUrl, exchange, 'add', { number1: 1, number2: 4 }).on('data', function(response) {
+  after(function () {
+    console.log('closing...');
+    return conn.close().then(() => console.log('done!'));
+  });
+
+  it('should send and receive a response', function (done) {
+    amqplibRpcClient(amqpUrl, exchange, 'add', { number1: 1, number2: 4 }).on('data', function (response) {
       expect(response.answer).to.be(5);
       done();
     });
